111ca2f15bebbdc62bd98e0219ecaab7f9996c37
synthet <dmn.synthet@gmail.com>
2014-03-19 11:06:33 +0400

    initial commit (desktop version)

---
+++ src/ru/synthet/synthpass/SynthPassDesktop.java
@@ -0,0 +1,33 @@
+package ru.synthet.synthpass;
+
+import java.awt.*;
+import java.applet.Applet;
+
+/**
+ * Created by bragin_va on 19.03.14.
+ */
+public class SynthPassDesktop extends Applet {
+
+    private final PassGenerator passGenerator = new PassGenerator();
+    private volatile String resultString = "password";
+    private String masterPassword = "123";
+    private String domainName;
+
+    public void paint (Graphics g) {
+        g.drawString(gen(), 50, 25);
+    }
+
+    private String gen() {
+        String inputString = masterPassword + domainName;
+        do {
+            resultString = passGenerator.synthEncrypt(inputString,
+                    PassGenerator.PassRules.generatedPasswordLength);
+            inputString = resultString;
+            //if (generationThread.isInterrupted()) {
+            //    break;
+            //}
+        } while (!passGenerator.validate(resultString));
+        return resultString;
+    }
+
+}
