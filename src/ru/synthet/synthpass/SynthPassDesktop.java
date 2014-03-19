de4e64fb2fe62024f47f995976af5d81c6e50279
synthet <dmn.synthet@gmail.com>
2014-03-19 11:17:19 +0400

    Swing init()

--- src/ru/synthet/synthpass/SynthPassDesktop.java
+++ src/ru/synthet/synthpass/SynthPassDesktop.java
@@ -1,20 +1,34 @@
 package ru.synthet.synthpass;
 
+import javax.swing.*;
 import java.awt.*;
-import java.applet.Applet;
 
-/**
- * Created by bragin_va on 19.03.14.
- */
-public class SynthPassDesktop extends Applet {
+public class SynthPassDesktop extends JApplet {
 
     private final PassGenerator passGenerator = new PassGenerator();
     private volatile String resultString = "password";
     private String masterPassword = "12345";
-    private String domainName;
+    private String domainName = "";
 
-    public void paint (Graphics g) {
-        g.drawString(gen(), 50, 25);
+    public void init() {
+        //Execute a job on the event-dispatching thread:
+        //creating this applet's GUI.
+        try {
+            javax.swing.SwingUtilities.invokeAndWait(new Runnable() {
+                public void run() {
+                    createGUI();
+                }
+            });
+        } catch (Exception e) {
+            System.err.println("createGUI didn't successfully complete");
+        }
+    }
+
+    public void createGUI () {
+        JLabel label = new JLabel(gen());
+        label.setHorizontalAlignment(JLabel.CENTER);
+        label.setBorder(BorderFactory.createMatteBorder(1,1,1,1, Color.black));
+        getContentPane().add(label, BorderLayout.CENTER);
     }
 
     private String gen() {
